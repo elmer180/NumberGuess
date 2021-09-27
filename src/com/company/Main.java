bed759a282b6d03b187256c49e5108a16c98d217
elmer180 <ronnieelmer05@gmail.com>
2021-09-27 10:05:43 +0100

    upd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,19 +6,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-    Random random = new Random();
-    int attempt =(1);
-    int guess = (0);
-    int ran = (random.nextInt(100)+1);
-    while(guess =! ran){
-        System.out.println("Pick a number between 1-100");
-        int guess = input.nextInt();
-        if (guess == ran){
-            System.out.println("Spot on you took"+attempt+"attempts");
+        Random random = new Random();
+        int attempt = (1);
+        int guess = (0);
+        int ran = (random.nextInt(100) + 1);
+        int hotl = (ran - 10);
+        int hoth = (ran + 10);
+        int warml = (ran - 20);
+        int warmh = (ran + 20);
+        while (guess != ran) {
+            System.out.println("Pick a number between 1-100");
+            guess = input.nextInt();
+            if (guess == ran) {
+                System.out.println("Spot on you took " + attempt + " attempts");
+            } else {
+                attempt = (attempt + 1);
+                if ((hoth > guess) && (hotl < guess)) {
+                    System.out.println("hot");
+                } else {
+                    if ((warmh > guess) && (warml < guess)) {
+                        System.out.println("warm");
+                    }
+
+                }
             }
-        else
-            attempt =(attempt+1);
-            //if
-        }}}
+        }
+    }
+}
